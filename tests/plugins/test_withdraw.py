cd6fef676288bf7623488b13471f17189f30ac36
Mendel Mobach <mendel@mobach.nl>
2023-12-07 10:38:23 +0100

    meer

---
+++ tests/plugins/test_withdraw.py
@@ -0,0 +1,53 @@
+from unittest.mock import Mock, patch
+import plugins.withdraw as withdraw_module
+
+def test_withdraw_constructor():
+    master_mock = Mock()
+    withdraw = withdraw_module.withdraw("SID", master_mock)
+    assert withdraw.SID == "SID"
+    assert withdraw.master == master_mock
+
+def test_withdraw_valid_amount():
+    master_mock = Mock()
+    withdraw = withdraw_module.withdraw("SID", master_mock)
+    master_mock.receipt = Mock()
+
+    # Adjust the valid amount range to pass the condition
+    assert withdraw.withdraw("500") == True
+    master_mock.receipt.add.assert_called_with(
+        True, 500.0, "Withdrawal or unlisted product", 1, None, "withdraw"
+    )
+
+def test_withdraw_invalid_amount():
+    master_mock = Mock()
+    withdraw = withdraw_module.withdraw("SID", master_mock)
+
+    # Test with an amount outside the valid range
+    assert withdraw.withdraw("10000") == True
+    master_mock.send_message.assert_called_with(
+        True,
+        "message",
+        "Enter an amount between 0.01 and 999.99 or scan a product"
+    )
+
+def test_withdraw_non_numeric_input():
+    master_mock = Mock()
+    withdraw = withdraw_module.withdraw("SID", master_mock)
+
+    assert withdraw.withdraw("not_a_number") is None
+
+def test_withdraw_input():
+    master_mock = Mock()
+    withdraw = withdraw_module.withdraw("SID", master_mock)
+
+    # Assuming input method is a placeholder with no functional code
+    withdraw.input("test_input")
+    # No assertion needed, just checking if method exists and runs without error
+
+def test_withdraw_startup():
+    master_mock = Mock()
+    withdraw = withdraw_module.withdraw("SID", master_mock)
+
+    # Assuming startup is a placeholder with no functional code
+    withdraw.startup()
+    # No assertion needed, just checking if method exists and runs without error
