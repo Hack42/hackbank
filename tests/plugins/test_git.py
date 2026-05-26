a585725ccd88beb2895d866026167f448d106b67
Mendel Mobach <mendel@mobach.nl>
2026-05-27 00:44:52 +0200

    Serialiseer background git commits

--- tests/plugins/test_git.py
+++ tests/plugins/test_git.py
@@ -1,6 +1,5 @@
 from unittest.mock import Mock, patch
 import plugins.git as git_module
-import threading
 
 
 class TestGit:
@@ -9,11 +8,18 @@ class TestGit:
         self.git = git_module.git("SID", self.master_mock)
 
     def test_background(self):
-        with patch("plugins.git.os.system") as mock_system:
+        with patch("plugins.git.subprocess.run") as mock_run:
             self.git.background()
-            mock_system.assert_called_with(
-                "cd data && git commit -m " + str(self.master_mock.transID) + " ."
+            mock_run.assert_called_with(
+                ["git", "commit", "-m", str(self.master_mock.transID), "."],
+                cwd="data",
+                check=False,
             )
+
+    def test_background_uses_shared_lock(self):
+        other = git_module.git("SID2", Mock())
+
+        assert self.git.lock is other.lock
 
     def test_hook_post_checkout(self):
         with patch("plugins.git.threading.Thread") as mock_thread:
